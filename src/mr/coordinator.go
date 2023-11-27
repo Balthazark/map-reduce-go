f4cd018b553e89aafa268f2e3dfcaad23c279ed5
hshanz <david00gbg@hotmail.com>
2023-11-27 16:20:47 +0000

    changed output

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -65,7 +65,7 @@ func (c *Coordinator) GetTask(args *TaskArgs, taskReply *TaskResponse) error {
         }
     }
     c.mutex.Unlock()
-    return errors.New("all tasks completed")
+    return errors.New("all tasks assigned waiting to finish")
 }
 
 
